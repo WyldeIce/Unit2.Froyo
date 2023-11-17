2d64b8659fb29c26f27f2a4735635a3c19ec6a13
WyldeIce <ssteinmetz@SethsPC>
2023-11-17 12:20:33 -0500

    ProjectDone

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let userInput = prompt(
+    "Please enter your order seperated by commas, no spaces",
+    "vanilla, chocolate, strawberry"
+  );
+
+// ^^^ Generate prompt on load ^^^
+userInput = userInput.replace(/\s/g, '');
+const stringArr = userInput.split(",");
+
+// ^^^ Divide the user input string into an array ^^^  --- Learned the replacement from freecodecamp :)
+
+const count = [];
+function countOrder(stringArr) {
+    for (let i = 0; i < stringArr.length; i++) {
+        let item = stringArr[i];
+        if(count[item] === undefined) {
+            count[item] = 0;
+        }
+        count[item]++;
+    }
+    return count;
+}
+
+console.log(countOrder(stringArr));
